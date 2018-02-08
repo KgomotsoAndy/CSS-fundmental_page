096344deef386bb5fa554a686272d2c885e1c48e
KgomotsoAndy <36259285+KgomotsoAndy@users.noreply.github.com>
2018-02-08 11:14:24 +0200

    This is my first site and it has a nice pictures of me
    This is my first web-site and has nice pictures of me. It doesn't have a more information right now but it will be more exciting website

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/lesego.jpg') {
+      myImage.setAttribute ('src','images/lesego2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/lesego.jpg');
+    }
+}
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+//User name code
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to my site!, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome back, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
